55daf9c3829d61070e36864b914402e0a18a7995
Srinivas-VJ <srinivasvj01@gmail.com>
2023-02-03 11:13:29 +0530

    cleaned up messages

--- chess-backend/src/main/java/com/example/chess/domain/User.java
+++ chess-backend/src/main/java/com/example/chess/domain/User.java
@@ -1,24 +1,23 @@
 package com.example.chess.domain;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.convert.DataSizeUnit;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    @Size(min = 3, max = 69)
-    @NotBlank(message = "username is required")
+    @Size(min = 3, max = 69, message = "User names must have a minimum size of 3 and a maximum size of 69")
+    @NotNull(message = "User name is a required field")
     private String userName;
-    @Email(message = "invalid email address")
+    @Email(message = "Invalid Email address")
+    @NotNull(message = "User email is a required field")
     private String userEmail;
     private String profilePicture;
     private int gamesWon = 0;
